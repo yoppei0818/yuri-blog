66c18d4774f6ab00f88ac7d76413c0bbe537ab99
yoppei0818 <yoppei0818@gmail.com>
2024-11-11 02:56:12 +0900

    #9 microCMSのlibファイルを整理

--- src/lib/microcms.ts
+++ src/lib/microcms.ts
@@ -7,12 +7,17 @@ const client = createClient({
   apiKey: import.meta.env.MICROCMS_API_KEY,
 });
 
-export type Blog = {
+// microCMS自動作成項目　型
+type SystemCreatedContent = {
   id: string;
   createdAt: string;
   updatedAt: string;
   publishedAt: string;
   revisedAt: string;
+};
+
+// 全コンテンツ共通型
+type BaseContent = SystemCreatedContent & {
   eyecatch: {
     url: string;
     height: number;
@@ -21,28 +26,42 @@ export type Blog = {
   eyecatch_alt: string;
   title: string;
   content: string;
-  tags: {
-    id: string;
-    createdAt: string;
-    updatedAt: string;
-    publishedAt: string;
-    revisedAt: string;
-    name: string;
-  }[];
+  tags: Tag[];
 };
-export type BlogResponse = {
+type BaseResponse<T> = {
   totalCount: number;
   offset: number;
   limit: number;
-  contents: Blog[];
+  contents: T[];
 };
 
-// !endpoint：blogs
-// blog一覧取得
+// ブログ　型
+export type Blog = BaseContent;
+export type BlogResponse = BaseResponse<Blog>;
+
+// 開発物　型
+export type Work = BaseContent & {
+  work_url: string;
+};
+export type WorkResponse = BaseResponse<Work>;
+
+// タグ　型
+export type Tag = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  name: string;
+};
+export type TagResponse = BaseResponse<Tag>;
+
+//!　endpoint：blogs
+// ブログ一覧取得
 export const getBlogs = async (queries?: MicroCMSQueries) => {
   return await client.get<BlogResponse>({ endpoint: "blogs", queries });
 };
-// blog詳細取得
+// ブログ詳細取得
 export const getBlogDetail = async (
   blogId: string,
   queries?: MicroCMSQueries
@@ -50,6 +69,40 @@ export const getBlogDetail = async (
   return await client.getListDetail<Blog>({
     endpoint: "blogs",
     contentId: blogId,
+    queries,
+  });
+};
+
+//!　endpoint：works
+// 開発物一覧取得
+export const getWorks = async (queries?: MicroCMSQueries) => {
+  return await client.get<WorkResponse>({ endpoint: "works", queries });
+};
+// 開発物詳細取得
+export const getWorkDetail = async (
+  workId: string,
+  queries?: MicroCMSQueries
+) => {
+  return await client.getListDetail<Work>({
+    endpoint: "works",
+    contentId: workId,
+    queries,
+  });
+};
+
+//!　endpoint：tags
+// タグ一覧取得
+export const getTags = async (queries?: MicroCMSQueries) => {
+  return await client.get<TagResponse>({ endpoint: "tags", queries });
+};
+// タグ詳細取得
+export const getTagDetail = async (
+  tagId: string,
+  queries?: MicroCMSQueries
+) => {
+  return await client.getListDetail<Tag>({
+    endpoint: "tags",
+    contentId: tagId,
     queries,
   });
 };
